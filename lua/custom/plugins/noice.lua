2555ad8f25b4e44ff7bd4b4065169792f22b9abf
Henry Zheng <henry.zheng727@gmail.com>
2026-05-12 19:50:18 -0600

    add noice plugin (for centered cmdline, mostly)

---
+++ lua/custom/plugins/noice.lua
@@ -0,0 +1,5 @@
+vim.pack.add { 'https://github.com/MunifTanjim/nui.nvim' }
+vim.pack.add { 'https://github.com/rcarriga/nvim-notify' }
+vim.pack.add { 'https://github.com/folke/noice.nvim' }
+
+require('noice').setup {}
